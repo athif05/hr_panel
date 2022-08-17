82068b2d3b0480f36b869940da4e5b3b3a76a884
athif05 <athif.hussain05@gmail.com>
2022-08-17 16:24:07 +0530

    alter user table

---
+++ database/migrations/2022_08_17_104341_add_some_new_column_user.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('company_id')->nullable();
+            $table->string('last_name')->nullable();
+            $table->unsignedBigInteger('company_location_id')->nullable();
+            $table->unsignedBigInteger('reporting_to_id')->nullable();
+            $table->enum('employee_type', ['Probation', 'Confirmed'])->default('Probation');
+            $table->date('date_of_confirmation')->nullable();
+            $table->date('due_date_of_confirmation')->nullable();
+            $table->date('appraisal_cycle')->nullable();
+            $table->string('gender')->nullable();
+            $table->foreign('company_id')->references('id')->on('company_names');
+            $table->foreign('company_location_id')->references('id')->on('company_locations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -171,13 +171,13 @@
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <!-- <img src="{{ asset('public/assests/assets/img/profile-img.jpg') }}" alt="Profile" class="rounded-circle"> -->
-            <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
+            <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->first_name }}</span>
           </a><!-- End Profile Iamge Icon -->
 
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>{{ Auth::user()->name }}</h6>
+              <h6>{{ Auth::user()->first_name }}</h6>
               <span>Web Designer</span>
             </li>
             <li>
