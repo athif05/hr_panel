c75a3479654d68fa6f14c8aac83fd8ecfd9b81ee
athif05 <athif.hussain05@gmail.com>
2022-09-09 17:32:34 +0530

    redirect home page from click on logo

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,7 +3,7 @@
   <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex ">
+      <a href="{{ url('/home')}}" class="logo d-flex ">
         @if(session('company_logo'))
           <img src="{{ asset('').session('company_logo') }}" alt="VCOne">
         @else
